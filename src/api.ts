8e5b36a9529e4b8c81f5bb184bb0a79bd76e8919
Jacob9606 <snm9606@gmail.com>
2024-02-04 23:52:45 +0900

    11 52pm

--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,30 @@
+/* const BASE_URL = `https://api.coinpaprika.com/v1`;
+
+export function fetchCoins() {
+  return fetch(`${BASE_URL}/coins`).then((response) => response.json());
+}
+
+export function fetchCoinInfo(coinId: string) {
+  return fetch(`${BASE_URL}/coins/${coinId}`).then((response) =>
+    response.json()
+  );
+}
+
+export function fetchCoinTickers(coinId: string) {
+  return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) =>
+    response.json()
+  );
+}
+
+export function fetchCoinHistory(coinId: string) {
+  const endDate = Math.floor(Date.now() / 1000);
+  const startDate = endDate - 60 * 60 * 24 * 7 * 2;
+  return fetch(
+    `${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`
+  ).then((response) => response.json());
+}
+ */
+
 const BASE_URL = `https://api.coinpaprika.com/v1`;
 
 export function fetchCoins() {
@@ -14,4 +41,13 @@ export function fetchCoinTickers(coinId: string) {
   return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) =>
     response.json()
   );
+}
+
+const CUSTOM_API_URL = `https://ohlcv-api.nomadcoders.workers.dev`;
+
+export function fetchCoinHistory(coinId: string) {
+  const endDate = Math.floor(Date.now() / 1000);
+  const startDate = endDate - 60 * 60 * 24 * 7 * 2;
+  const apiUrl = `${CUSTOM_API_URL}?coinId=${coinId}&start=${startDate}&end=${endDate}`;
+  return fetch(apiUrl).then((response) => response.json());
 }
